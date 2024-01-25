10cf40e20ea3c4e531d2c9052eaaaddc56aa60c8
michalani <michal.anisimow@gmail.com>
2024-01-25 01:24:37 +0000

    restricted to /s path more optimal performance

--- main.user.js
+++ main.user.js
@@ -1,27 +1,26 @@
 // ==UserScript==
 // @name         Amazon Sum Sold Items
 // @namespace    amazon-sum-sold-items
-// @version      2024-01-25
-// @description  Sum sold items when visitng a page
+// @version      2024-01-26
+// @description  Sum sold items for a visited amazon store page
 // @author       You
-// @include     /^https?:\/\/(www|smile).amazon\.*\/*
+// @include      /^https?:\/\/(www|smile).amazon\.*\/*[a-z.]*\/s
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.co.uk
-// @updateURL   https://raw.githubusercontent.com/michalani/amazon-sum-sold-items/main/main.user.js
-// @downloadURL https://raw.githubusercontent.com/michalani/amazon-sum-sold-items/main/main.user.js
+// @updateURL    https://raw.githubusercontent.com/michalani/amazon-sum-sold-items/main/main.user.js
+// @downloadURL  https://raw.githubusercontent.com/michalani/amazon-sum-sold-items/main/main.user.js
 // @grant        none
 // ==/UserScript==
 // 2024-01-24 - Initial Version
 // 2024-01-25 - Improved regex / fixed to account for abbreviation in K
+// 2024-01-26 - Restricted script to /s path
 
-// var regexPattern = /\b(\d+)\+/g;
+let regexPattern = /^\d+K?\++ bought in past month/
 
-var regexPattern = /^\d+K?\++ bought in past month/
-
-var z = document.querySelectorAll("span.a-size-base.a-color-secondary");
+let itemsSold = document.querySelectorAll("span.a-size-base.a-color-secondary");
 
 var totalSum = 0;
 
-z.forEach(function(item) {
+itemsSold.forEach(function(item) {
     var matches = item.textContent.match(regexPattern);
 
     if (matches) {
@@ -35,8 +34,6 @@ z.forEach(function(item) {
                     totalSum += number;
                 }
             }
-
-            
         });
     }
 });
@@ -44,4 +41,4 @@ z.forEach(function(item) {
 // 'totalSum' now contains the sum of all numbers matching the regex pattern.
 document.querySelector("div.s-no-outline > span:nth-child(1)").textContent = "Results (Sold "+totalSum+")"
 
-console.log(totalSum);
+//console.log(totalSum);
